f01dae9661ce69b4917b6fc1b9d726cd4dbb949a
DST-ftp-root <chendon@dstonline.com>
2018-03-01 17:11:01 -0500

    automated file upload

--- test.bash
+++ test.bash
@@ -1,7 +1,7 @@
-filenamevar1="2018_March_01"
+filenamevar1="strg001.out.2018_March_01"
 errStr=""
 
-if [ ! -f /home/ec2-user/$filenamevar1* ]; then
+if [ -f "/home/ec2-user/OptGeoData/2018/March/01/$filenamevar1*" ]; then
         reportErr=true
         errStr+=" "$filenamevar1
 fi
